cdfe3717daeee952e597804c87aab6182eff8b52
Marko Banusic <mbanusic@gmail.com>
2017-03-02 16:13:37 +0100

    Add initial migrations and User Model

--- src/UserManagementServiceProvider.php
+++ src/UserManagementServiceProvider.php
@@ -13,7 +13,8 @@ class UserManagementServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     /**
