4da25194c02e01751be2d9a426415767b17ac212
Marko Banusic <mbanusic@gmail.com>
2017-04-05 16:52:39 +0200

    Add Auth Controller and routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,22 +1,27 @@
 <?php
 
+/*
+ * list of user related routes
+ *
+ *  - /login - POST|GET
+ *  - /logout - GET
+ *  - /user/{id} - POST|GET|PUT
+ */
+
+
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 Route::group(['middleware' => ['api']], function (Request $request) {
 
+    Route::any('login', function() {});
+    Route::any('logout', function() {});
 
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
+    Route::group(['middleware' =>'auth:api'], function() {
+        Route::get('user/{id?}', function ($id) {})->where(['id' => '[0-9]+']); // get create user form or user edit form
+        Route::post('user', function() {}); // post create user form
+        Route::put('user/{id}', function($id) {}); //update user form
     });
+
 
 });
