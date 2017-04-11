b114d906d9210b4b672937c38ae22fff3986b830
Marko Banusic <mbanusic@gmail.com>
2017-04-11 20:36:18 +0200

    Modify complete package to enable registration and login

--- src/Controllers/Auth/RegisterController.php
+++ src/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace Inspirium\UserManagement\Controllers\Auth;
 
-use App\User;
+use Inspirium\UserManagement\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
